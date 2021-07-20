2ca10462440b4404b53114be7f8dd82d5d27d1f0
Sem_Ir <semi.r04.sr@gmail.com>
2021-07-20 23:02:36 +0500

    Deleted comments

--- BruteForce.py
+++ BruteForce.py
@@ -1,22 +1,21 @@
-# Взлом пароля методом BruteForce. Сделал Sem_Ir
+# Взлом пароля методом BruteForce
 import random
 
-correctPassword = '1234'  # Здесь стоит верный пароль
-wrongPasswords = []     # Сюда будут складываться неправильные пароли
-password = ''       # Сюда будет складываться сгенерированный пароль
-length = 4      # Длинна пароля
-chars = '1234567890'        # Список генерируемых чисел
-run = True
+correctPassword = '1234'  
+wrongPasswords = []     
+password = ''       
+length = 4     
+chars = '1234567890'        
 
 while run:
-    password = ''       # Обнуление переменной
+    password = ''       
     for i in range(length):
-        password += random.choice(chars)    # Генерация паролей и добавление их в переменную password
-    if password not in wrongPasswords:      # Если пароля нет в переменной wrongPasswords то выводим на экран
+        password += random.choice(chars)    
+    if password not in wrongPasswords:      
         print(password)
         if password != correctPassword:
-            wrongPasswords.append(password)     # Если пароль неправильный добавляем в переменную wrongPasswords
+            wrongPasswords.append(password)    
         else:
             print(password + ' is correct')
             run = False
-            break       # Если пароль правильный то выводится на экран и цикл прерывается
+            break       
--- encrypt.py
+++ encrypt.py
@@ -1,23 +1,22 @@
 # дешифратор
 
 numbers0 = input('Введите значение: ') # юзер введет сюда значение (например - 1001 или 0110)
-numbers = list(numbers0) # создаем из список из переменной numbers0
+numbers = list(numbers0) 
 run = True
 
-# После проверим есть ли другие цифры
+# проверим есть ли другие цифры
 for number in numbers:
     if int(number) > 1 or int(number) < 0:
         print('Error!')
         run = False
         break
 
-# Теперь начинаем расшифрование
+# расшифрование
 if run == True:
-    numbers.reverse() # переворачиваем список
-    length = len(numbers) # длина списка
+    numbers.reverse() 
+    length = len(numbers) 
     n = 0 
     nums = [] # здесь будут значения из следующего цикла
-    # Здесь уже начинается расшифрование по школьной формуле
     for number in numbers:
         if n != length:
             nums.append(int(number) * 2**n)
